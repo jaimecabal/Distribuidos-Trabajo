2ae3057b9e6366bb9871c71eee25f5e7bf86c8ba
Jaime Cabal <jcabalf3@gmail.com>
2022-12-31 20:12:41 +0100

    Commit inicial

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author jaimecabalfernandez
+ *
+ */
+module Distribuidos_Trabajo {
+}
---
+++ src/trabajo/Servidor.java
@@ -0,0 +1,23 @@
+package trabajo;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Servidor {
+	public static void main(String[] args) {
+		try {
+			ServerSocket ss = new ServerSocket(8080);
+			ExecutorService pool = Executors.newCachedThreadPool();
+			while(true) {
+				Socket s = ss.accept();
+				pool.execute(new AtenderPeticion(s));
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
