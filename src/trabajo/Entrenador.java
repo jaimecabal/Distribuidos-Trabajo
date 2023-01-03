5cd652b9392222fe4264769854ebdfec3c07105c
jaimecabal <jcabalf3@gmail.com>
2023-01-03 10:19:54 +0100

    Merge branch 'main' of https://github.com/jaimecabal/Distribuidos-Trabajo into main

--- src/trabajo/Entrenador.java
+++ src/trabajo/Entrenador.java
@@ -1,21 +1,5 @@
 package trabajo;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-
-import clases.Equipo;
-import clases.Pokemon;
-
 public class Entrenador {
 	public static void main(String[] args) {
 //		System.out.println("Escoge introduce los pokemon que se van a añadir al equipo:");
